4d5e1fa1a702dd73b6ef1baf5ff8c77f08d8bf52
Rich Boakes <rjb@port.ac.uk>
2023-08-03 11:56:15 +0100

    Add publish tools, fix #7

---
+++ publish.js
@@ -0,0 +1,19 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'node:url';
+import metalsmith from 'metalsmith';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const ms = metalsmith(__dirname);
+
+ms.source('./build/');
+ms.destination('./publish/');
+ 
+ms.build(function (err) {
+  if (err) {
+    console.log('Borked!');
+    throw err;
+  }
+});
+
+console.log('Published!');
