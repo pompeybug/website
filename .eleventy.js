181e1ec7b5b1e3101ff94c85b0ac9afa9501c360
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-09-20 12:48:22 +0100

    fix: copy all images from articles over

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const readingTime = require('eleventy-plugin-reading-time');
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = function(config) {
     config.addPassthroughCopy("src/assets");
+    config.addPassthroughCopy("src/articles/**/*.{jpg,jpeg,png,pdf,svg,webp}")
 
     // enable reading time estimates
     config.addPlugin(readingTime);
