4d78760bd17c7f9e06d45652317d9af5f5dbbe56
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-09-23 14:11:08 +0100

    feat: add todos collection

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,11 @@ const readingTime = require('eleventy-plugin-reading-time');
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = function(config) {
     config.addPassthroughCopy("src/assets");
-    config.addPassthroughCopy("src/articles/**/*.{jpg,jpeg,png,pdf,svg,webp}")
+    config.addPassthroughCopy("src/articles/**/*.{jpg,jpeg,png,pdf,svg,webp}");
+
+    config.addCollection('todoCollection', (collectionApi) => {
+        return collectionApi.getAll().filter((item) => item.data.todo);
+    });
 
     // enable reading time estimates
     config.addPlugin(readingTime);
