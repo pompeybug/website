4f144d54e501fd838b53ada576a6c48c705eb6d4
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-09-30 00:02:52 +0100

    add sitemap, robots and prefetch on hover

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pcf.boakes.org",
-  integrations: [mdx()],
+  integrations: [mdx(), robotsTxt(), sitemap(), prefetch()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
+    remarkPlugins: [remarkReadingTime]
+  }
 });
