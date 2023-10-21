280459ec9da866b7bae81b2ad20103f047b6bc71
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-10-21 22:47:19 +0100

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,24 @@ import mdx from "@astrojs/mdx";
 import { remarkReadingTime, remarkStripMarkdown } from "./remark-plugins.mjs";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pcf.boakes.org",
-  integrations: [mdx(), robotsTxt(), sitemap(), prefetch()],
+  integrations: [
+    mdx(),
+    robotsTxt(),
+    sitemap(),
+    prefetch(),
+    partytown({
+      config: {
+        forward: "dataLayer.push",
+      },
+    }),
+  ],
   markdown: {
-    remarkPlugins: [remarkReadingTime, remarkStripMarkdown]
-  }
+    remarkPlugins: [remarkReadingTime, remarkStripMarkdown],
+  },
 });
