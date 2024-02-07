802167968c09d66d0ee1a3cb9d2ada325e49d7e1
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-07 20:30:44 +0000

    improve args

--- sync_content.sh
+++ sync_content.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if [ "$1" != '--pages' ]; then
+PAGES=0
+HTTP=0
+
+for arg in "${@}"; do
+  if [ "$arg" == "--pages" ]; then
+    PAGES=1
+  elif [ "$arg" == "--http" ]; then
+    HTTP=1
+  fi
+done
+
+if [ $PAGES -eq 0 ]; then
   rm -rf src/content/*/
 
   if [ ! -d "content" ]; then
-    git clone git@github.com:pompeybug/content.git content
+    if [ $HTTP -eq 1 ]; then
+      git clone https://github.com/pompeybug/website.git content
+    else
+      git clone git@github.com:pompeybug/content.git content
+    fi
   else
     git -C content pull -q
   fi
