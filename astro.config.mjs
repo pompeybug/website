121222737ca0c334c90eb98b98dd0648b286cd3e
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-09 21:14:02 +0000

    add astro-critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,23 +7,18 @@ import partytown from "@astrojs/partytown";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pompeybug.co.uk",
-  integrations: [
-    mdx(),
-    robotsTxt(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: "dataLayer.push",
-      },
-    }),
-    svelte(),
-    icon(),
-  ],
+  integrations: [mdx(), robotsTxt(), sitemap(), partytown({
+    config: {
+      forward: "dataLayer.push"
+    }
+  }), svelte(), icon(), critters()],
   markdown: {
-    remarkPlugins: [remarkReadingTime, remarkStripMarkdown],
+    remarkPlugins: [remarkReadingTime, remarkStripMarkdown]
   },
-  prefetch: true,
+  prefetch: true
 });
