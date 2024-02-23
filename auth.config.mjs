0fbedfb66d34c2a08c68c77f45dde6980ce880c4
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-23 13:53:47 +0000

    add organisation member check to login

--- auth.config.mjs
+++ auth.config.mjs
@@ -6,10 +6,35 @@ export default defineConfig({
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-      authorization: { params: { scope: 'repo read:org' } }
+      authorization: { params: { scope: "repo read:org" } },
     }),
   ],
   callbacks: {
+    async signIn({ profile, account }) {
+      if (!profile?.id || !account?.access_token) {
+        return false;
+      }
+
+      const organisationMembers = await fetch(
+        `https://api.github.com/orgs/${
+          import.meta.env.GITHUB_ORGANISATION
+        }/members`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${account.access_token}`,
+          },
+        }
+      );
+
+      if (!organisationMembers.ok) {
+        return false;
+      }
+
+      const body = await organisationMembers.json();
+
+      return body.some((member) => member.id === profile.id);
+    },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
       return session;
