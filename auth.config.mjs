d3ca79e2b8aea121290f613c599498aa95598a2c
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-23 13:15:14 +0000

    add access token to token and add scopes

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,11 +1,25 @@
-import GitHub from '@auth/core/providers/github';
-import { defineConfig } from 'auth-astro';
+import GitHub from "@auth/core/providers/github";
+import { defineConfig } from "auth-astro";
 
 export default defineConfig({
   providers: [
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+      authorization: { params: { scope: 'repo read:org' } }
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async token({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+
+      return token;
+    },
+  },
 });
