a8b05bc2ebd90fa74e7d22edd2808945f75d1880
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-24 10:55:47 +0000

    add accesstoken type to auth package

---
+++ src/app.d.ts
@@ -0,0 +1,9 @@
+import type { DefaultSession } from "@auth/core/types";
+
+declare module "@auth/core/types" {
+  interface Session {
+    user?: DefaultSession['user'];
+    expires: DefaultSession['expires'];
+    accessToken: string;
+  }
+}
