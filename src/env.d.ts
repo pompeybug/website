0cc6a451da5fed38dc99dd721bf5cc0b5c463e56
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-24 10:55:30 +0000

    update env files

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,10 +2,16 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-    readonly DOMAIN: string;
-    readonly HTTPS: boolean;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly DOMAIN: string;
+  readonly HTTPS: boolean;
+  readonly GITHUB_CLIENT_ID: string;
+  readonly GITHUB_CLIENT_SECRET: string;
+  readonly AUTH_TRUST_HOST: boolean;
+  readonly AUTH_SECRET: string;
+  readonly GITHUB_ORGANISATION: string;
+  readonly GITHUB_CONTENT_REPOSITORY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
