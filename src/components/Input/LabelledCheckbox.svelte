c905150b8978d9d9062af1f99367b8772cba8562
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-29 09:58:08 +0000

    add labelled checkbox input

---
+++ src/components/Input/LabelledCheckbox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let label: string;
+  export let id: string;
+  export let name = id;
+  export let checked = false;
+  export let inputElement: HTMLInputElement | undefined = undefined;
+</script>
+
+<div class="checkbox">
+  <label for={id}>{label}</label>
+  <input {id} {name} type="checkbox" bind:checked bind:this={inputElement} />
+</div>
+
+<style>
+  .checkbox {
+    display: flex;
+    align-items: center;
+    gap: calc(var(--fixedspace) * 0.5);
+  }
+
+  .checkbox input {
+    margin: 0;
+  }
+</style>
