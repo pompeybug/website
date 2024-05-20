f6cf9888673478a9628c05bcc33c904842ebb086
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-05-20 12:32:54 +0100

    use nanostores to store session on each page globally

---
+++ src/lib/sessionStore.ts
@@ -0,0 +1,14 @@
+import { computed, map } from "nanostores";
+import type { Session } from "@lib/types";
+
+// This store only persists for the current page, it will not persist when the user loads a new page.
+// Regardless, this is still useful if the webpage needs to check the session multiple times.
+// This is because it means only one request in the NavMenu.astro component is needed (this component is always loaded).
+// Components within the rest of the page can setup a listener to these stores and update the page accordingly (see Article.astro).
+
+// nanostores doesn't allow null values, empty object is the next best option
+export const $session = map<Session | {}>();
+
+export const $sessionIsValid = computed($session, (session) => {
+  return Object.values(session).length !== 0;
+});
