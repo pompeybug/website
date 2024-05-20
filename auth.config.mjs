67c0fa6ba6bd49fbcaf946419e67f32abd0c0658
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-05-20 12:34:38 +0100

    add signIn callback to validate github org membership

--- auth.config.mjs
+++ auth.config.mjs
@@ -9,4 +9,37 @@ export default defineConfig({
       authorization: { params: { scope: "repo read:org" } },
     }),
   ],
+  callbacks: {
+    async signIn({ profile, account }) {
+      if (!profile?.id || !account?.access_token) {
+        return false;
+      }
+
+      let organisationMembersRes;
+
+      try {
+        organisationMembersRes = await fetch(
+          `https://api.github.com/orgs/${
+            import.meta.env.GITHUB_ORGANISATION
+          }/members`,
+          {
+            headers: {
+              Authorization: `Bearer ${account.access_token}`,
+            },
+          }
+        );
+      } catch (err) {
+        console.error(err);
+        return false;
+      }
+
+      if (!organisationMembersRes.ok) {
+        return false;
+      }
+
+      const organisationMembers = await organisationMembersRes.json();
+
+      return organisationMembers.some((member) => member.id === profile.id);
+    },
+  },
 });
